8c471baf1191ce3b13660ceb6559678c0160a337
carolinecollaco <carolinecollaco@gmail.com>
2015-04-16 22:17:07 -0300

    tentando resolver e não conseguindo

--- Encontro4/src/Exer3.java
+++ Encontro4/src/Exer3.java
@@ -3,16 +3,32 @@ import java.util.Scanner;
 public class Exer3 {
 
 	public static void main(String[] args) {
-		System.out.println("Digite a");		
-		Scanner num = new Scanner(System.in);		
-		int input = num.nextInt();
-		int a = delta(input);
-
+		Scanner entrada = new Scanner(System.in);		
+		int a;
+		System.out.println("Digite a");
+		a = entrada.nextInt();
+		System.out.println("O valor de a é:" + a);
+		
+		int b;
+		System.out.println("Digite b");
+		b = entrada.nextInt();
+		System.out.println("O valor de a é:" + b);
+		System.out.println("Digite c");
+		int c;
+		c = entrada.nextInt();
+		System.out.println("O valor de a é:" + c);
+				
+		entrada.close();
+		
+		int resultado = delta(a,b,c);
+		
+		System.out.println("O resultado é: " + resultado);
 	}
+		
 	
 	public static int delta(int a, int b, int c){
 		
-		int resultadodeDelta = b^2 - (4*a*b);
+		int resultadodeDelta = b^2 - (4*a*c);
 
 		return resultadodeDelta;
 	}
